ff49f0b622eda69acc558a9c73e7d4805168b3fa
im-sivaprakash <sivaprakash138@gmail.com>
2025-09-08 09:12:08 +0530

    fix

--- src/Components/Wishes.tsx
+++ src/Components/Wishes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../../firebase.config"; // Update this path
 import { collection, getDocs } from "firebase/firestore";
 import "./Wishes.css";
